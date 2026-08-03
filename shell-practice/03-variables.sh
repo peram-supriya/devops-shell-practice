56a27aba07debacdfa9c74b5c47dbe6d829e194a
peram-supriya <perampriya26@gmail.com>
2026-08-03 17:11:25 +0530

    actions

---
+++ shell-practice/03-variables.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+PERSON1=ravi
+PERSON2=ramu
+
+echo "$PERSON1:: hi, $PERSON2 how are you?"
+echo "$PERSON2:: hi $PERSON1, i am fine. how are you?"
+echo "$PERSON1:: i am also fine. what are you doing?"
+echo "$PERSON2:: i am learning devops. what about you?"
+echo "$PERSON1:: i am also learning devops. lets learn together."
