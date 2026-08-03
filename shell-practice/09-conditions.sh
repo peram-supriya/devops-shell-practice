1fca0a40c978b4e462a707f3d68b2fe633688737
peram-supriya <perampriya26@gmail.com>
2026-08-03 22:21:31 +0530

    actions

--- shell-practice/09-conditions.sh
+++ shell-practice/09-conditions.sh
@@ -5,6 +5,8 @@ NUMBER="$1"
 
 if [ $NUMBER -gt 20 ]; then
     echo "given $NUMBER is greater than 20"
+elif [ $NUMBER -eq 20 ]; then
+    echo "given $NUMBER is equal to 20"
 else
     echo "given $NUMBER is less than 20"
 fi
