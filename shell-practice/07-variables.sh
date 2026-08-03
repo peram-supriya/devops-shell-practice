437e434589568045c0d1b39bc8a8f7c37bfc2982
peram-supriya <perampriya26@gmail.com>
2026-08-03 19:12:18 +0530

    actions

--- shell-practice/07-variables.sh
+++ shell-practice/07-variables.sh
@@ -1,12 +1,12 @@
 #1/bin/bash
 
-echo "all variables are passed: $@"
+echo "all args passed to script: $@"
 echo "number of variables passed: $#"
 echo "script name: $0"
-echo "present working directory: $pwd"
+echo "present working directory: $PWD"
 echo "who is running this script: $USER"
 echo "Home directory of the user: $HOME"
 echo "PID of the script: $$"
-echo "sleep 1 &"
+sleep 2 &
 echo "background process id: $!"
 
