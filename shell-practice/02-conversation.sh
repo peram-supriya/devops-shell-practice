cc68dd47747487a21e0289524b7d6451e5561196
peram-supriya <perampriya26@gmail.com>
2026-08-03 15:53:17 +0530

    action

---
+++ shell-practice/02-conversation.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo "suresh:: hi, ramesh how are you?"
+echo "ramesh:: hi suresh, i am fine. how are you?"
+echo "suresh:: i am also fine. what are you doing?"
+echo "ramesh:: i am learning devops. what about you?"
+echo "suresh:: i am also learning devops. lets learn together."
