9a4e7c184c88c5a95c305dfb395364dfd3c81b42
peram-supriya <perampriya26@gmail.com>
2026-08-03 21:55:49 +0530

    actions

--- shell-practice/08-datatypes.sh
+++ shell-practice/08-datatypes.sh
@@ -7,4 +7,6 @@ SUM=$(($NUM1+$NUM2))
 echo "sum is $SUM"
 
 FRUITS=("Apple" "banana" "mango")
-echo "fruits are${FRUITS[@]}"
+echo "fruits are ${FRUITS[0]}"
+echo "fruits are ${FRUITS[1]}"
+echo "fruits are ${FRUITS[2]}"
